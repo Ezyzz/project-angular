dfffbbe61cf4f407e296a0fb914cf3b562a6a972
ntw_gan <6331305014@lamduan.mfu.ac.th>
2024-03-25 17:03:50 +0700

    better create new card UI

--- src/app/data/company-data.ts
+++ src/app/data/company-data.ts
@@ -4,11 +4,11 @@ import { CompanyModel } from '../models/company-model';
     { id: 1, name: 'Product 1', switch: true},
     { id: 2, name: 'Product 2', switch: false},
     { id: 3, name: 'Product 3', switch: true},
-    { id: 4, name: 'Product 1', switch: false},
-    { id: 5, name: 'Product 2', switch: true},
-    { id: 6, name: 'Product 3', switch: false},
-    { id: 7, name: 'Product 1', switch: true},
-    { id: 8, name: 'Product 2', switch: false},
-    { id: 9, name: 'Product 3', switch: true},
+    { id: 4, name: 'Dragonflyfromsky', switch: false},
+    { id: 5, name: 'DangerousDiagonalDispenser', switch: true},
+    { id: 6, name: 'Lorem ipsum dolor', switch: false},
+    { id: 7, name: 'Just number 1', switch: true},
+    { id: 8, name: '2', switch: false},
+    { id: 9, name: 'Test', switch: true},
   ];
   
